a49b66ab887dfbd5b98b3ebeb4480d134a2e7276
Igor Gomes <igorgomes0903@gmail.com>
2024-03-10 22:08:21 -0300

    conection from mongodb with repository and service

---
+++ src/main/java/com/igorgomes/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.igorgomes.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.igorgomes.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+	
+}
