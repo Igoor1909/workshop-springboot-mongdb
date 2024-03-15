6299aad6a959d3ef6d9395940294e3e64df1bacb
Igor Gomes <igorgomes0903@gmail.com>
2024-03-15 11:27:00 -0300

    acrescentando comentarios aos posts

--- src/main/java/com/igorgomes/workshopmongo/config/Instantiation.java
+++ src/main/java/com/igorgomes/workshopmongo/config/Instantiation.java
@@ -1,7 +1,6 @@
 package com.igorgomes.workshopmongo.config;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.TimeZone;
 
@@ -12,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.igorgomes.workshopmongo.domain.Post;
 import com.igorgomes.workshopmongo.domain.User;
 import com.igorgomes.workshopmongo.dto.AuthorDTO;
+import com.igorgomes.workshopmongo.dto.CommentDTO;
 import com.igorgomes.workshopmongo.repository.PostRepository;
 import com.igorgomes.workshopmongo.repository.UserRepository;
 
@@ -41,7 +41,12 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null,sdf.parse("21/03/2018") , "Partiu viagem!", "Vou viajar para São Paulo!", new AuthorDTO(maria) );
 		Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 		
+		CommentDTO c1 =  new CommentDTO("Boa viagem mano", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("19/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um bom dia", sdf.parse("23/03/2023"), new AuthorDTO(alex));
 		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
--- src/main/java/com/igorgomes/workshopmongo/domain/Post.java
+++ src/main/java/com/igorgomes/workshopmongo/domain/Post.java
@@ -1,13 +1,16 @@
 package com.igorgomes.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.igorgomes.workshopmongo.dto.AuthorDTO;
+import com.igorgomes.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,7 +24,8 @@ public class Post implements Serializable {
 	private String title;
 	private String body;
 	private AuthorDTO author;
-
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	public Post() {
 		
 	}
@@ -79,6 +83,14 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -92,8 +104,10 @@ public class Post implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
+}
+
 
 	
 	
 	
-}
+
