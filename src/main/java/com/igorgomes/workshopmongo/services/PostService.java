b12df4523fbb537bcd96a4655f634ca0d740c501
Igor Gomes <igorgomes0903@gmail.com>
2024-03-15 11:12:03 -0300

    obtendo post por ID

---
+++ src/main/java/com/igorgomes/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.igorgomes.workshopmongo.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.igorgomes.workshopmongo.domain.Post;
+import com.igorgomes.workshopmongo.repository.PostRepository;
+import com.igorgomes.workshopmongo.services.exception.ObjectoNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Post user = repo.findById(id).orElse(null);
+		if (user == null) {
+			throw new ObjectoNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+
+}
